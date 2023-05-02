41409d4c3af4e0421bd3780965b1af251b6ee70c
bwzm2009 <38091825+bwzm2009@users.noreply.github.com>
2023-05-02 07:44:13 -0400

    Update mh_symlink.h

--- mh_symlink.h
+++ mh_symlink.h
@@ -1,91 +1,38 @@
-/* Copyright (C) 2004 J.F.Dockes
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the
- *   Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
 #ifndef _MH_SYMLINK_H_INCLUDED_
 #define _MH_SYMLINK_H_INCLUDED_
 
-#include <string>
-#include "safeunistd.h"
-#include <errno.h>
-
-#include "cstr.h"
 #include "mimehandler.h"
-#include "transcode.h"
-#include "pathut.h"
-#include "log.h"
-
-/** Index symlink target 
- *
- * Not sure that this is such a good idea, so it's disabled by default in
- * the config. Add inode/symlink = internal to the index section of mimeconf 
- * to enable.
- */
-class MimeHandlerSymlink : public RecollFilter {
-public:
-    MimeHandlerSymlink(RclConfig *cnf, const std::string& id) 
-        : RecollFilter(cnf, id) {}
-    virtual ~MimeHandlerSymlink() = default;
-    MimeHandlerSymlink(const MimeHandlerSymlink&) = delete;
-    MimeHandlerSymlink& operator=(const MimeHandlerSymlink&) = delete;
-#ifdef _WIN32
+#include <string>
 #include <windows.h>
 
-std::string read_symlink_windows(const char* path) {
-    wchar_t wTargetPath[MAX_PATH];
-    HANDLE hFile = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT, NULL);
-    if (GetFinalPathNameByHandleW(hFile, wTargetPath, MAX_PATH, VOLUME_NAME_DOS) > 0) {
-        char targetPath[MAX_PATH];
-        WideCharToMultiByte(CP_UTF8, 0, wTargetPath, -1, targetPath, MAX_PATH, NULL, NULL);
-        CloseHandle(hFile);
-        return std::string(targetPath);
+class MimeHandlerSymlink : public RecollFilter {
+ public:
+    MimeHandlerSymlink(RclConfig *cnf, const std::string &id)
+        : RecollFilter(cnf, id) {}
+    virtual ~MimeHandlerSymlink() {}
+    virtual bool is_data_input_ok(DataInput input) const {
+        return input == DOCUMENT_FILE_NAME;
     }
-    CloseHandle(hFile);
-    return std::string();
-}
-#endif
+    virtual bool next_document();
+    virtual void clear() {}
 
-    virtual bool next_document() {
-        if (m_havedoc == false)
-            return false;
-        m_havedoc = false; 
-        m_metaData[cstr_dj_keycontent] = cstr_null;
-        char lc[1024];
-        std::string targetPath = read_symlink(m_fn);
-		ssize_t bytes = targetPath.length();
-
-        if (bytes != (ssize_t)-1) {
-            string slc(lc, bytes);
-            transcode(path_getsimple(slc), m_metaData[cstr_dj_keycontent], 
-                      m_config->getDefCharset(true), "UTF-8");
+ protected:
+    std::string read_symlink_windows(const std::string &path) {
+        std::string targetPath(MAX_PATH, '\0');
+        DWORD nchars = GetFinalPathNameByHandleA(
+            CreateFileA(path.c_str(), GENERIC_READ,
+                        FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr,
+                        OPEN_EXISTING,
+                        FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+                        nullptr),
+            &targetPath[0], MAX_PATH, FILE_NAME_NORMALIZED);
+        if (nchars > 0) {
+            targetPath.resize(nchars);
         } else {
-            LOGDEB("Symlink: readlink [" << m_fn << "] failed, errno " <<
-                   errno << "\n");
+            targetPath.clear();
         }
-        m_metaData[cstr_dj_keymt] = cstr_textplain;
-        return true;
+        return targetPath;
     }
-protected:
-    virtual bool set_document_file_impl(const string&, const string& fn) {
-        m_fn = fn;
-        return m_havedoc = true;
-    }
-
-private:
-    std::string m_fn;
 };
 
 #endif /* _MH_SYMLINK_H_INCLUDED_ */
-
