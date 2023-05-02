cc8113b09df83a0657f80732b7981c5f6233a0af
bwzm2009 <38091825+bwzm2009@users.noreply.github.com>
2023-05-02 09:06:10 -0400

    Update mh_symlink.h

--- mh_symlink.h
+++ mh_symlink.h
@@ -1,3 +1,19 @@
+/* Copyright (C) 2004 J.F.Dockes
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 #ifndef _MH_SYMLINK_H_INCLUDED_
 #define _MH_SYMLINK_H_INCLUDED_
 
@@ -6,7 +22,7 @@
 #include <windows.h>
 
 class MimeHandlerSymlink : public RecollFilter {
- public:
+public:
     MimeHandlerSymlink(RclConfig *cnf, const std::string &id)
         : RecollFilter(cnf, id) {}
     virtual ~MimeHandlerSymlink() {}
@@ -15,6 +31,7 @@ class MimeHandlerSymlink : public RecollFilter {
     }
     virtual bool next_document();
     void clear() {}
+    ...
 
  protected:
     std::string read_symlink_windows(const std::string &path) {
